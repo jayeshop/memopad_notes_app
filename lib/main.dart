28e59e3733aad5b7b8c31151ffc19800443b5bd8
jayeshop <jayeshumatheju@gmail.com>
2025-01-15 03:52:43 +0530

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/screens/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Notes App",
+      theme: ThemeData(
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        appBarTheme: AppBarTheme(color: Colors.black87),
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
