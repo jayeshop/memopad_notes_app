4b029a42cbc5335b8800d87de1521f8792789f7c
jayeshop <jayeshumatheju@gmail.com>
2025-01-15 17:21:39 +0530

    feat: Implement basic UI for Add/Edit Note Screen
    Implemented the basic UI for the Add/Edit Note Screen, including:
    - Setting the background color to white.
    - Adding an AppBar with a title based on whether it's a new note or an edit.
    - Initializing form fields and color selection.
    - Pre-filling fields with existing note data if editing.

--- lib/screens/add_edit_screen.dart
+++ lib/screens/add_edit_screen.dart
@@ -1,15 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/model/notes_model.dart';
+import 'package:notes_app/services/database_helper.dart';
 
 class AddEditNoteScreen extends StatefulWidget {
-  const AddEditNoteScreen({super.key});
+  final Note? note;
+  const AddEditNoteScreen({this.note});
 
   @override
   State<AddEditNoteScreen> createState() => _AddEditNoteScreenState();
 }
 
 class _AddEditNoteScreenState extends State<AddEditNoteScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final _titleController = TextEditingController();
+  final _contentController = TextEditingController();
+  final DatabaseHelper _databaseHelper = DatabaseHelper();
+  Color _selectedColor = Colors.amber;
+  final List<Color> _colors = [
+    Colors.amber,
+    Color(0xFF50C878),
+    Colors.redAccent,
+    Colors.blueAccent,
+    Colors.indigo,
+    Colors.purpleAccent,
+    Colors.pinkAccent
+  ];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    if (widget.note != null) {
+      _titleController.text = widget.note!.title;
+      _contentController.text = widget.note!.content;
+      _selectedColor = Color(int.parse(widget.note!.color));
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+        title: Text(widget.note == null ? "Add Note" : "Edit Note"),
+      ),
+    );
   }
 }
